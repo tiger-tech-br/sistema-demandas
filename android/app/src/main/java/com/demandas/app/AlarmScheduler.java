3d94b4a5df3b33d4aa2ee123e55ae6456a1346fd
Jose Andrey <134113125+J-Andrey@users.noreply.github.com>
2026-07-12 16:51:50 -0300

    alarm

--- android/app/src/main/java/com/demandas/app/AlarmScheduler.java
+++ android/app/src/main/java/com/demandas/app/AlarmScheduler.java
@@ -25,8 +25,8 @@ public class AlarmScheduler {
 
         Calendar calendar = Calendar.getInstance();
 
-        calendar.set(Calendar.HOUR_OF_DAY, 20);
-        calendar.set(Calendar.MINUTE, 8);
+        calendar.set(Calendar.HOUR_OF_DAY, 16);
+        calendar.set(Calendar.MINUTE, 55);
         calendar.set(Calendar.SECOND, 0);
 
         // Se já passou das 10h hoje, agenda para amanhã
